e627c311a3c834e6e6c331ba136f3b49f25ec04d
Edward Jeselnik <eddie@jeselnik.xyz>
2023-08-17 20:53:18 +1000

    update

--- .config/nvim/lua/lualine-conf.lua
+++ .config/nvim/lua/lualine-conf.lua
@@ -2,6 +2,7 @@ require('lualine').setup{
   options = {
     theme = 'base16',
     section_separators = '',
-    component_separators = ''
+    component_separators = '',
+    icons_enabled = false
   }
 }
